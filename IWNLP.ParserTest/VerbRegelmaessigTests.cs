3eb9fea836f94ca89dc98beca108cd554763f7b5
Liebeck <Liebeck@users.noreply.github.com>
2015-11-17 16:29:58 +0100

    VerbRegelmaessigTests: Added 6 unit tests

--- IWNLP.ParserTest/VerbRegelmaessigTests.cs
+++ IWNLP.ParserTest/VerbRegelmaessigTests.cs
@@ -189,6 +189,234 @@ namespace IWNLP.ParserTest
             CollectionAssert.AreEqual(expectedWords, words, "failed");
         }
 
+        [TestMethod]
+        public void regnen()
+        {
+            String word = "regnen";
+            int wikiID = 427337;
+            String text = DumpTextCaching.GetTextFromPage(wikiID);
+
+            WiktionaryParser parser = new WiktionaryParser();
+            List<Models.Entry> words = parser.ParseText(word, text, wikiID);
+            List<VerbConjugation> expectedWords = new List<VerbConjugation>()
+            {
+                 new VerbConjugation()
+                 {
+                   Text = word,
+                   WiktionaryID = wikiID,
+                    PräsensAktivIndikativ_Singular3Person = new List<string>(){"regnet"},
+                    PräsensAktivIndikativ_Plural3Person = new List<string>(){"regnen"},
+                    PräteritumAktivIndikativ_Singular3Person = new List<string>(){"regnete"},
+                    PräteritumAktivIndikativ_Plural3Person = new List<string>(){"regneten"},
+                    PartizipII = "geregnet"
+                 }
+            };
+            XMLSerializer.Serialize<List<Models.Entry>>(expectedWords.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_1.txt"));
+            XMLSerializer.Serialize<List<Models.Entry>>(words.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_2.txt"));
+            CollectionAssert.AreEqual(expectedWords, words, "failed");
+        }
+
+        [TestMethod]
+        public void durchixen()
+        {
+            String word = "durchixen";
+            int wikiID = 153011;
+            String text = DumpTextCaching.GetTextFromPage(wikiID);
+
+            WiktionaryParser parser = new WiktionaryParser();
+            List<Models.Entry> words = parser.ParseText(word, text, wikiID);
+            List<VerbConjugation> expectedWords = new List<VerbConjugation>()
+            {
+                 new VerbConjugation()
+                 {
+                   Text = word,
+                    WiktionaryID = wikiID,
+                    PräsensAktivIndikativ_Singular1Person = new List<string>(){"ixe durch"},
+                    PräsensAktivIndikativ_Singular1Person_Nebensatzkonjugation = new List<string>(){"durchixe"},
+                    PräsensAktivIndikativ_Singular2Person = new List<string>(){"ixst durch"},
+                    PräsensAktivIndikativ_Singular2Person_Nebensatzkonjugation = new List<string>(){"durchixst"},
+                    PräsensAktivIndikativ_Singular3Person = new List<string>(){"ixt durch"},
+                    PräsensAktivIndikativ_Singular3Person_Nebensatzkonjugation = new List<string>(){"durchixt"},
+                    PräsensAktivIndikativ_Plural1Person = new List<string>(){"ixen durch"},
+                    PräsensAktivIndikativ_Plural1Person_Nebensatzkonjugation = new List<string>(){"durchixen"},
+                    PräsensAktivIndikativ_Plural2Person = new List<string>(){"ixt durch"},
+                    PräsensAktivIndikativ_Plural2Person_Nebensatzkonjugation = new List<string>(){"durchixt"},
+                    PräsensAktivIndikativ_Plural3Person = new List<string>(){"ixen durch"},      
+                    PräsensAktivIndikativ_Plural3Person_Nebensatzkonjugation = new List<string>(){"durchixen"},
+                    PräteritumAktivIndikativ_Singular1Person = new List<string>(){"ixte durch"},
+                    PräteritumAktivIndikativ_Singular1Person_Nebensatzkonjugation = new List<string>(){"durchixte"},
+                    PräteritumAktivIndikativ_Singular2Person  = new List<string>(){"ixtest durch"},
+                    PräteritumAktivIndikativ_Singular2Person_Nebensatzkonjugation  = new List<string>(){"durchixtest"},
+                    PräteritumAktivIndikativ_Singular3Person  = new List<string>(){"ixte durch"},
+                    PräteritumAktivIndikativ_Singular3Person_Nebensatzkonjugation  = new List<string>(){"durchixte"},
+                    PräteritumAktivIndikativ_Plural1Person  = new List<string>(){"ixten durch"},
+                    PräteritumAktivIndikativ_Plural1Person_Nebensatzkonjugation  = new List<string>(){"durchixten"},
+                    PräteritumAktivIndikativ_Plural2Person =  new List<string>(){"ixtet durch"},
+                    PräteritumAktivIndikativ_Plural2Person_Nebensatzkonjugation =  new List<string>(){"durchixtet"},
+                    PräteritumAktivIndikativ_Plural3Person  = new List<string>(){"ixten durch"},
+                    PräteritumAktivIndikativ_Plural3Person_Nebensatzkonjugation  = new List<string>(){"durchixten"},
+                    PartizipII = "durchgeixt"
+                 },
+            };
+            XMLSerializer.Serialize<List<Models.Entry>>(expectedWords.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_1.txt"));
+            XMLSerializer.Serialize<List<Models.Entry>>(words.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_2.txt"));
+            CollectionAssert.AreEqual(expectedWords, words, "failed");
+        }
+
+        [TestMethod]
+        public void gewittern()
+        {
+            String word = "gewittern";
+            int wikiID = 427338;
+            String text = DumpTextCaching.GetTextFromPage(wikiID);
+
+            WiktionaryParser parser = new WiktionaryParser();
+            List<Models.Entry> words = parser.ParseText(word, text, wikiID);
+            List<VerbConjugation> expectedWords = new List<VerbConjugation>()
+            {
+                 new VerbConjugation()
+                 {
+                   Text = word,
+                   WiktionaryID = wikiID,
+                    PräsensAktivIndikativ_Singular3Person = new List<string>(){"gewittert"},
+                    PräteritumAktivIndikativ_Singular3Person = new List<string>(){"gewitterte"},
+                    PartizipII = "gewittert"
+                 }
+            };
+            XMLSerializer.Serialize<List<Models.Entry>>(expectedWords.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_1.txt"));
+            XMLSerializer.Serialize<List<Models.Entry>>(words.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_2.txt"));
+            CollectionAssert.AreEqual(expectedWords, words, "failed");
+        }
+
+        [TestMethod]
+        public void gesundschrumpfen()
+        {
+            String word = "gesundschrumpfen";
+            int wikiID = 348585;
+            String text = DumpTextCaching.GetTextFromPage(wikiID);
+
+            WiktionaryParser parser = new WiktionaryParser();
+            List<Models.Entry> words = parser.ParseText(word, text, wikiID);
+            List<VerbConjugation> expectedWords = new List<VerbConjugation>()
+            {
+                 new VerbConjugation()
+                 {
+                   Text = word,
+                    WiktionaryID = wikiID,
+                    PräsensAktivIndikativ_Singular1Person = new List<string>(){"schrumpfe gesund"},
+                    PräsensAktivIndikativ_Singular1Person_Nebensatzkonjugation = new List<string>(){"gesundschrumpfe"},
+                    PräsensAktivIndikativ_Singular2Person = new List<string>(){"schrumpfst gesund","schrumpfest gesund"},
+                    PräsensAktivIndikativ_Singular2Person_Nebensatzkonjugation = new List<string>(){"gesundschrumpfst","gesundschrumpfest"},
+                    PräsensAktivIndikativ_Singular3Person = new List<string>(){"schrumpft gesund","schrumpfet gesund"},
+                    PräsensAktivIndikativ_Singular3Person_Nebensatzkonjugation = new List<string>(){"gesundschrumpft","gesundschrumpfet"},
+                    PräsensAktivIndikativ_Plural1Person = new List<string>(){"schrumpfen gesund"},
+                    PräsensAktivIndikativ_Plural1Person_Nebensatzkonjugation = new List<string>(){"gesundschrumpfen"},
+                    PräsensAktivIndikativ_Plural2Person = new List<string>(){"schrumpft gesund","schrumpfet gesund"},
+                    PräsensAktivIndikativ_Plural2Person_Nebensatzkonjugation = new List<string>(){"gesundschrumpfet"},
+                    PräsensAktivIndikativ_Plural3Person = new List<string>(){"schrumpfen gesund"},      
+                    PräsensAktivIndikativ_Plural3Person_Nebensatzkonjugation = new List<string>(){"gesundschrumpfen"},
+                    PräteritumAktivIndikativ_Singular1Person = new List<string>(){"schrumpfte gesund","schrumpfete gesund"},
+                    PräteritumAktivIndikativ_Singular1Person_Nebensatzkonjugation = new List<string>(){"gesundschrumpfte","gesundschrumpfete"},
+                    PräteritumAktivIndikativ_Singular2Person  = new List<string>(){"schrumpftest gesund","schrumpfetest gesund"},
+                    PräteritumAktivIndikativ_Singular2Person_Nebensatzkonjugation  = new List<string>(){"gesundschrumpftest","gesundschrumpfetest"},
+                    PräteritumAktivIndikativ_Singular3Person  = new List<string>(){"schrumpfte gesund","schrumpfete gesund"},
+                    PräteritumAktivIndikativ_Singular3Person_Nebensatzkonjugation  = new List<string>(){"gesundschrumpfte","gesundschrumpfete"},
+                    PräteritumAktivIndikativ_Plural1Person  = new List<string>(){"schrumpften gesund","schrumpfeten gesund"},
+                    PräteritumAktivIndikativ_Plural1Person_Nebensatzkonjugation  = new List<string>(){"gesundschrumpften","gesundschrumpfeten"},
+                    PräteritumAktivIndikativ_Plural2Person =  new List<string>(){"schrumpftet gesund","schrumpfetet gesund"},
+                    PräteritumAktivIndikativ_Plural2Person_Nebensatzkonjugation =  new List<string>(){"gesundschrumpftet","gesundschrumpfetet"},
+                    PräteritumAktivIndikativ_Plural3Person  = new List<string>(){"schrumpften gesund","schrumpfeten gesund"},
+                    PräteritumAktivIndikativ_Plural3Person_Nebensatzkonjugation  = new List<string>(){"gesundschrumpften","gesundschrumpfeten"},
+                    PartizipII = "gesundgeschrumpft"
+                 },
+            };
+            XMLSerializer.Serialize<List<Models.Entry>>(expectedWords.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_1.txt"));
+            XMLSerializer.Serialize<List<Models.Entry>>(words.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_2.txt"));
+            CollectionAssert.AreEqual(expectedWords, words, "failed");
+        }
+
+        [TestMethod]
+        public void in_Stand_setzen()
+        {
+            String word = "in Stand setzen";
+            int wikiID = 427336;
+            String text = DumpTextCaching.GetTextFromPage(wikiID);
+
+            WiktionaryParser parser = new WiktionaryParser();
+            List<Models.Entry> words = parser.ParseText(word, text, wikiID);
+            List<VerbConjugation> expectedWords = new List<VerbConjugation>()
+            {
+                 new VerbConjugation()
+                 {
+                   Text = word,
+                    WiktionaryID = wikiID,
+                   PräsensAktivIndikativ_Singular1Person = new List<string>(){"setze in Stand"},
+                   PräsensAktivIndikativ_Singular2Person = new List<string>(){"setzt in Stand"},
+                   PräsensAktivIndikativ_Singular3Person = new List<string>(){"setzt in Stand","setzet in Stand"},
+                   PräsensAktivIndikativ_Plural1Person = new List<string>(){"setzen in Stand"},
+                   PräsensAktivIndikativ_Plural2Person = new List<string>(){"setzt in Stand","setzet in Stand"},
+                   PräsensAktivIndikativ_Plural3Person = new List<string>(){"setzen in Stand"},      
+                    PräteritumAktivIndikativ_Singular1Person = new List<string>(){"setzte in Stand","setzete in Stand"},
+                    PräteritumAktivIndikativ_Singular2Person  = new List<string>(){"setztest in Stand","setzetest in Stand"},
+                    PräteritumAktivIndikativ_Singular3Person  = new List<string>(){"setzte in Stand","setzete in Stand"},
+                    PräteritumAktivIndikativ_Plural1Person  = new List<string>(){"setzten in Stand","setzeten in Stand"},
+                    PräteritumAktivIndikativ_Plural2Person =  new List<string>(){"setztet in Stand","setzetet in Stand"},
+                    PräteritumAktivIndikativ_Plural3Person  = new List<string>(){"setzten in Stand","setzeten in Stand"},
+                    PartizipII = "in Stand gesetzt"
+                 },
+            };
+            XMLSerializer.Serialize<List<Models.Entry>>(expectedWords.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_1.txt"));
+            XMLSerializer.Serialize<List<Models.Entry>>(words.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_2.txt"));
+            CollectionAssert.AreEqual(expectedWords, words, "failed");
+        }
+
+        [TestMethod]
+        public void zufriedenstellen()
+        {
+            String word = "zufriedenstellen ";
+            int wikiID = 442128;
+            String text = DumpTextCaching.GetTextFromPage(wikiID);
+
+            WiktionaryParser parser = new WiktionaryParser();
+            List<Models.Entry> words = parser.ParseText(word, text, wikiID);
+            List<VerbConjugation> expectedWords = new List<VerbConjugation>()
+            {
+                 new VerbConjugation()
+                 {
+                   Text = word,
+                    WiktionaryID = wikiID,
+                    PräsensAktivIndikativ_Singular1Person = new List<string>(){"stelle zufrieden"},
+                    PräsensAktivIndikativ_Singular1Person_Nebensatzkonjugation = new List<string>(){"zufriedenstelle"},
+                    PräsensAktivIndikativ_Singular2Person = new List<string>(){"stellst zufrieden"},
+                    PräsensAktivIndikativ_Singular2Person_Nebensatzkonjugation = new List<string>(){"zufriedenstellst"},
+                    PräsensAktivIndikativ_Singular3Person = new List<string>(){"stellt zufrieden"},
+                    PräsensAktivIndikativ_Singular3Person_Nebensatzkonjugation = new List<string>(){"zufriedenstellt"},
+                    PräsensAktivIndikativ_Plural1Person = new List<string>(){"stellen zufrieden"},
+                    PräsensAktivIndikativ_Plural1Person_Nebensatzkonjugation = new List<string>(){"zufriedenstellen"},
+                    PräsensAktivIndikativ_Plural2Person = new List<string>(){"stellt zufrieden"},
+                    PräsensAktivIndikativ_Plural2Person_Nebensatzkonjugation = new List<string>(){"zufriedenstellt"},
+                    PräsensAktivIndikativ_Plural3Person = new List<string>(){"stellen zufrieden"},      
+                    PräsensAktivIndikativ_Plural3Person_Nebensatzkonjugation = new List<string>(){"zufriedenstellen"},
+                    PräteritumAktivIndikativ_Singular1Person = new List<string>(){"stellte zufrieden"},
+                    PräteritumAktivIndikativ_Singular1Person_Nebensatzkonjugation = new List<string>(){"zufriedenstellte"},
+                    PräteritumAktivIndikativ_Singular2Person  = new List<string>(){"stelltest zufrieden"},
+                    PräteritumAktivIndikativ_Singular2Person_Nebensatzkonjugation  = new List<string>(){"zufriedenstelltest"},
+                    PräteritumAktivIndikativ_Singular3Person  = new List<string>(){"stellte zufrieden"},
+                    PräteritumAktivIndikativ_Singular3Person_Nebensatzkonjugation  = new List<string>(){"zufriedenstellte"},
+                    PräteritumAktivIndikativ_Plural1Person  = new List<string>(){"stellten zufrieden"},
+                    PräteritumAktivIndikativ_Plural1Person_Nebensatzkonjugation  = new List<string>(){"zufriedenstellten"},
+                    PräteritumAktivIndikativ_Plural2Person =  new List<string>(){"stelltet zufrieden"},
+                    PräteritumAktivIndikativ_Plural2Person_Nebensatzkonjugation =  new List<string>(){"zufriedenstelltet"},
+                    PräteritumAktivIndikativ_Plural3Person  = new List<string>(){"stellten zufrieden"},
+                    PräteritumAktivIndikativ_Plural3Person_Nebensatzkonjugation  = new List<string>(){"zufriedenstellten"},
+                    PartizipII = "zufriedengestellt"
+                 },
+            };
+            XMLSerializer.Serialize<List<Models.Entry>>(expectedWords.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_1.txt"));
+            XMLSerializer.Serialize<List<Models.Entry>>(words.Cast<Models.Entry>().ToList(), System.IO.Path.Combine(AppSettingsWrapper.UnitTestDumpDirectory, "verb_2.txt"));
+            CollectionAssert.AreEqual(expectedWords, words, "failed");
+        }
+
 
     }
 }
