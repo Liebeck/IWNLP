5a129b68cff8d72a55186ac5142a232a04331609
Liebeck <Liebeck@users.noreply.github.com>
2015-12-08 03:33:27 +0100

    cleanup

--- IWNLP.Parser/POSParser/ParameterAdjectivalDeclension.cs
+++ IWNLP.Parser/POSParser/ParameterAdjectivalDeclension.cs
@@ -11,15 +11,5 @@ namespace IWNLP.Parser.POSParser
         public const String Parameter1 = "1";
         public const String KeinSingular = "kein Singular";
         public const String KeinPlural = "kein Plural";
-
-        //public const String NominativSingularSchwach = "Nominativ Singular schwach";
-        //public const String GenitivSingularSchwach = "Genitiv Singular schwach";
-        //public const String DativSingularSchwach = "Dativ Singular schwach";
-        //public const String AkkusativSingularSchwach = "Akkusativ Singular schwach";
-
-        //public const String NominativPluralSchwach = "Nominativ Plural schwach";
-        //public const String GenitivPluralSchwach = "Genitiv Plural schwach";
-        //public const String DativPluralSchwach = "Dativ Plural schwach";
-        //public const String AkkusativPluralSchwach = "Akkusativ Plural schwach";
     }
 }
