12c424b7957a92541a7e131810f204caeb3743c8
Liebeck <Liebeck@users.noreply.github.com>
2016-10-21 19:28:02 +0200

    Expand the blacklist

--- IWNLP.Parser/GlobalBlacklist.cs
+++ IWNLP.Parser/GlobalBlacklist.cs
@@ -19,6 +19,8 @@ namespace IWNLP.Parser
             "Flexion:uzen",
             "Flexion:wischen",           
             "Seiner Majestät Schiff", // fixed after 20161001 dump
+            "chemisches Element", // fixed after 20161001 dump
+            "Wunderbares"
         };
 
         public static List<String> SuppressError = new List<string>()
