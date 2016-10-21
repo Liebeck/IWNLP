4c7500f67c55292fee24e3dda1a961d7ed37b36e
Liebeck <Liebeck@users.noreply.github.com>
2016-10-21 10:44:20 +0200

    Add class for the template 'Deutsch Substantiv Übersicht -sch'

---
+++ IWNLP.Parser/POSParser/DeutschSubstantivUebersichtParser.cs
@@ -0,0 +1,28 @@
+﻿using IWNLP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWNLP.Parser.POSParser
+{
+    public class DeutschSubstantivUebersichtParser : ParserBase
+    {
+        public Word Parse(String word, String[] text) 
+        {
+            List<String> cleanedTemplateBlock = this.GetCleanedTemplateBlock(word, text);
+            return null;
+        }
+
+        public List<String> GetCleanedTemplateBlock(String word, String[] text)
+        {
+            int flexionSubstantivStart = text.Select((content, index) => new { Content = content.Trim(), Index = index }).Where(x => x.Content.Contains("{{Deutsch Substantiv Übersicht -sch")).Select(x => x.Index).First();
+            int flexionSubstantivEnd = text.Select((content, index) => new { Content = content.Trim(), Index = index }).Where(x => x.Index >= flexionSubstantivStart && x.Content.EndsWith("}}")).Select(x => x.Index).First();
+            String[] definition = Common.GetSubArray(text, flexionSubstantivStart, flexionSubstantivEnd);
+            List<String> cleanedLines = base.GetCleanedMultilineDefintionBlock(definition, word, "DeutschSubstantivUebersichtParser");
+            return cleanedLines;
+        }
+
+    }
+}
