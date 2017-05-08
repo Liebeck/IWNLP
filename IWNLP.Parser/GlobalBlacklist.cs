463fe1d55897ba23635f656c1fb3769a0ada6453
Liebeck <Liebeck@users.noreply.github.com>
2017-05-08 14:41:57 +0200

    Remove entries from the blacklist that are fixed

--- IWNLP.Parser/GlobalBlacklist.cs
+++ IWNLP.Parser/GlobalBlacklist.cs
@@ -13,26 +13,7 @@ namespace IWNLP.Parser
             "Oachkatzlschwoaf",
             "Flexion:anbaggern",
             "Flexion:husten",
-            "Flexion:uzen",
-            "Flexion:wischen", // fixed after 20161020 dump          
-            "Seiner Majestät Schiff", // fixed after 20161020 dump
-            "chemisches Element", // fixed after 20161020 dump
-            "Wunderbares", // fixed after 20161020 dump
-            "Brauner Knollenblätterpilz", // fixed after 20161020 dump
-            "Neues", // fixed after 20161020 dump
-            "Unparteiischer", // fixed after 20161020 dump
-            "Schutzbefohlener", // fixed after 20161020 dump
-            "Weiße", // fixed after 20161020 dump
-            "Tadig", // fixed after 20161020 dump
-            "Unserdeutsch", // fixed after 20161020 dump
-            "Hessisch",// fixed after 20161020 dump
-            "Portokosten", // fixed after 20161020 dump
-            "Abokosten", // fixed after 20161020 dump
-            "Rocky Mountains", // fixed after 20161020 dump
-            "Shetlandinseln", // fixed after 20161020 dump
-            "Manx", // fixed after 20161020 dump
-            "Flexion:verwehren", // fixed after 20161020 dump
-            "Flexion:gestatten", // fixed after 20161020 dump
+            "Flexion:uzen"
         };
     }
 }
